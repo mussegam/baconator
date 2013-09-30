4837c8ac4a088598c6b3fc82870fb994414b4b6e
mussegam <austorm@gmail.com>
2013-09-30 10:06:30 +0200

    adding the code to consume twitter streaming api. sending the websocket clients the bacon lover data

--- src/clj/baconator/server/handler.clj
+++ src/clj/baconator/server/handler.clj
@@ -1,25 +1,66 @@
 (ns baconator.server.handler
   (:use compojure.core
         ring.util.response
-        org.httpkit.server)
+        org.httpkit.server
+        twitter.callbacks
+        twitter.callbacks.handlers)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.middleware.reload :as reload]
             [cheshire.core :refer :all]
-            [org.httpkit.client :as http]))
+            [clojure.edn :as edn]
+            [org.httpkit.client :as http]
+            [twitter.oauth :as oauth]
+            [twitter.api.streaming :as api])
+  (:import (twitter.callbacks.protocols AsyncStreamingCallback)))
+
+(declare send-checkin)
+
+;; Twitter streaming api
+
+(def twitter-config (edn/read-string (slurp "config/twitter.edn")))
+(def twitter-creds (oauth/make-oauth-creds (:consumer-key twitter-config)
+                                           (:consumer-secret twitter-config)
+                                           (:token twitter-config)
+                                           (:token-secret twitter-config)))
+
+(def ^:dynamic *callback*
+  (AsyncStreamingCallback.
+   (fn [_resp payload]
+      (let [tweet (parse-string (str payload) true)
+            lower (.toLowerCase (:text tweet))]
+        (when (:coordinates tweet)
+          (println (:text tweet))
+          (let [coordinates (:coordinates (:coordinates tweet))
+                user (:screen_name (:user tweet))]
+            (send-checkin {:user user
+                           :text (:text tweet)
+                           :lat (nth coordinates 1)
+                           :lon (nth coordinates 0)})))))
+   (fn [_resp]
+      (println "Error connecting to twitter streaming API"))
+   (fn [_resp excp]
+      (.printStackTrace excp))))
+
+(api/statuses-filter :params {:track "bacon"}
+                     :oauth-creds twitter-creds
+                     :callbacks *callback*)
+
+;; Handle the checkins websocket
 
 (def checkins-clients (atom {}))
+
 (defn checkins [req]
   (with-channel req con
     (swap! checkins-clients assoc con true)
     (on-close con (fn [status]
                      (swap! checkins-clients dissoc con)))))
 
-(future (loop []
-          (doseq [client @checkins-clients]
-            (send! (key client) (pr-str {:lat (rand 40) :lon (rand 5)}) false))
-          (Thread/sleep 2000)
-          (recur)))
+(defn send-checkin [msg]
+  (doseq [client @checkins-clients]
+    (send! (key client) (pr-str msg) false)))
+
+;; Basic ring/compojure code
 
 (defroutes app-routes
   (GET "/checkins" [] checkins)
