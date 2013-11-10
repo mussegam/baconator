559b45c1fc27f5aca202d4340c0d3837d2bd62f9
mussegam <austorm@gmail.com>
2013-11-10 01:42:24 +0100

    updating the dependencies

--- project.clj
+++ project.clj
@@ -4,26 +4,28 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.0"]
-                 [ring/ring-devel "1.2.0"]
+                 [org.clojure/clojurescript "0.0-2024"]
+                 [com.taoensso/timbre "2.7.1"]
+                 [ring/ring-core "1.2.1"]
+                 [ring/ring-devel "1.2.1"]
                  [ring/ring-json "0.2.0"]
-                 [compojure "1.1.5"]
-                 [http-kit "2.1.10"]
+                 [compojure "1.1.6"]
+                 [http-kit "2.1.11"]
                  [twitter-api "0.7.4"]
                  [cheshire "5.2.0"]
-                 [org.clojure/clojurescript "0.0-1859"]
-                 [com.taoensso/timbre "2.6.3"]]
-  :plugins [[lein-cljsbuild "0.3.3"]
-            [lein-ring "0.8.7"]]
+                 [javax.servlet/servlet-api "2.5"]
+                 [org.clojure/tools.reader "0.7.10"]]
+  :plugins [[lein-cljsbuild "1.0.0-alpha2"]
+            [lein-ring "0.8.8"]]
   :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
   :cljsbuild {
               :builds {
                        :main {
                               :source-paths ["src/cljs"]
-                              :compiler {:output-to "resources/public/js/main.js"
-                                         :optimizations :simple
-                                         :pretty-print true}
+                              :compiler {:optimizations :simple
+                                         :pretty-print false
+                                         :output-to "resources/public/js/main.js"}
                               :jar true}}}
   :profiles {:uberjar {:main baconator.server.handler :aot :all}}
   :aot :all
